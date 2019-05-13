27d7382fc2b87238d680c4f32a92c61a285e1dd4
Art Karma <inste@icloud.com>
2019-05-13 20:14:21 +0300

    first commit

--- ConstrainsWithCodePart2/ViewController.swift
+++ ConstrainsWithCodePart2/ViewController.swift
@@ -9,12 +9,49 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let blueView: UIView = {
+        let myView = UIView()
+        myView.backgroundColor = .blue
+        myView.translatesAutoresizingMaskIntoConstraints = false
+        return myView
+    }()
+    
+    let redView: UIView = {
+        let myView = UIView()
+        myView.backgroundColor = .red
+        myView.translatesAutoresizingMaskIntoConstraints = false
+        return myView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.addSubview(blueView)
+        view.addSubview(redView)
+        
+        let viewVFL = ["redView": redView, "blueView": blueView]
+        let metrics = ["height": 100, "width": (view.bounds.width / 3), "myTop": (view.bounds.height / 2)]
+        
+        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-myTop-[redView(height)]|",
+                                                           options: [], // внутренние параметры объекта (отступы, центровка(left, right) и тд)
+                                                           metrics: metrics,
+                                                           views: viewVFL))
+        
+        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-myTop-[blueView(height)]|",
+                                                           options: [],
+                                                           metrics: metrics,
+                                                           views: viewVFL))
+        
+        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-[redView(width)]-50-[blueView(width)]-|",
+                                                           options: [],
+                                                           metrics: metrics,
+                                                           views: viewVFL))
+        
+        
     }
-
-
+    
+    
+    
 }
 
